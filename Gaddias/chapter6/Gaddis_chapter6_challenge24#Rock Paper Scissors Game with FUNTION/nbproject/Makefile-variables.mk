840cc190079789c98e4d60a3a9d10cec65d06dc0
yeti1215 <yeti527120@gmail.com>
2022-04-14 14:38:12 -0700

    Signed-off-by: yeti1215 <yeti527120@gmail.com>

---
+++ Gaddias/chapter6/Gaddis_chapter6_challenge24#Rock Paper Scissors Game with FUNTION/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_ARTIFACT_NAME_Debug=gaddis_chapter6_challenge24_rock_paper_scissors_game_with_funtion
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/gaddis_chapter6_challenge24_rock_paper_scissors_game_with_funtion
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_PACKAGE_NAME_Debug=gaddischapter6challenge24#rockpaperscissorsgamewithfuntion.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/gaddischapter6challenge24#rockpaperscissorsgamewithfuntion.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_ARTIFACT_NAME_Release=gaddis_chapter6_challenge24_rock_paper_scissors_game_with_funtion
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/gaddis_chapter6_challenge24_rock_paper_scissors_game_with_funtion
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_PACKAGE_NAME_Release=gaddischapter6challenge24#rockpaperscissorsgamewithfuntion.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/gaddischapter6challenge24#rockpaperscissorsgamewithfuntion.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
--- Savitch/chapter3/savitch_chapter3_problem7#value by sum/main.cpp
+++ Savitch/chapter3/savitch_chapter3_problem7#value by sum/main.cpp
@@ -12,34 +12,31 @@
 #include <iomanip> //bring the format library
 
 using namespace std;
+float eachvalue(int,int);
+
 
 
 int main(int argc, char** argv) {
-
+  
+    float sum=1; //the initial sum value is 1;  
     int x, n;
-    float sum=1; //the initial sum value is 1;
-    char ch;
-    
-    
-    do{
-        cout<<"Enter the value of X: "<<endl;
-          cin>>x;
-        cout<<"Enter the value of n between 1 to 100:"<<endl;
-          cin>>n;
-        
-        for(int i=1; i<=n; i++){
-            sum += pow(x,i)/i;
+    cout << "Enter the value of X: " << endl;
+    cin>>x;
+    cout << "Enter the value of n between 1 to 100:" << endl;
+    cin>>n;
+          for(int i=1; i <=n; i++)
+          {
+            sum += eachvalue(x,i);
             cout<<fixed<<setprecision(2)<<showpoint;
-            cout<<"n= "<<i<<" e^x = "<<sum<<"\t";
+            cout<<sum<<"  ";
             if(i%10==0)cout<<endl;
-        }
-        cout<<endl;
-        
-        cout<<"Do you want repeat the calculate, Y/N? "<<endl;
-        cin.get(ch);
-    }
-    while(ch=='y' && ch=='Y');
-    
+          }
+     
+   
     return 0;
 }
 
+float eachvalue(int x, int i){
+    float eachV= pow(x,i)/i; //calculate each x^n/n value and return the value
+           return eachV;
+}
